ba7f646664b433b7e37c34e77ce6d2e458dcd47c
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-02 09:38:20 +0200

    Update and rename set.py to get_pretrained.py

--- get_pretrained.py
+++ get_pretrained.py
@@ -2,29 +2,6 @@ import os
 import requests
 from tqdm import tqdm
 
-'''
-def download(url: str, dest_folder: str, new_filename: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)  # create folder if it does not exist
-
-    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
-    file_path = os.path.join(dest_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-    else:  # HTTP status code 4XX/5XX
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text)) 
-    
-    new_file_path = os.path.join(dest_folder, new_filename)    
-    os.rename(file_path, new_file_path)    
-'''
 
 def download(url: str, dest_folder: str, new_filename: str, chunk_size=1024):
     filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
