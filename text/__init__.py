343f56375dfb890b58cdf65761df7e22683c1179
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-02 09:29:48 +0200

    Add files via upload

--- text/__init__.py
+++ text/__init__.py
@@ -1,73 +1,59 @@
+""" from https://github.com/keithito/tacotron """
 from text import cleaners
-from text.symbols import symbols, _pad
-import os
-from dp.phonemizer import Phonemizer
-phonemizer = Phonemizer.from_checkpoint('en_us_cmudict_ipa_forward.pt')
+from text.symbols import symbols
+
 
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}
 
-def do_ipa(text):
-    text = text.split()
-    new_text = []
-    for t in text:
-        if t.startswith('*'):
-            t = t.replace("*", "")
-            new_text.append(t)
-        else:    
-            t = phoneme_text(t)
-            new_text.append(t)
-    
-    return ' '.join(new_text)
 
-
-def phoneme_text(text, lang='en_us'):
-    text = phonemizer(text, lang)
-    return text.strip()
-
-def text_to_sequence(text, symbols, cleaner_names):
+def text_to_sequence(text, cleaner_names):
+    """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+      text: string to convert to a sequence
+      cleaner_names: names of the cleaner functions to run the text through
+    Returns:
+      List of integers corresponding to the symbols in the text
+    """
     sequence = []
-    symbol_to_id = {s: i for i, s in enumerate(symbols)}
+
     clean_text = _clean_text(text, cleaner_names)
-    print(clean_text)
-    clean_text = phoneme_text(clean_text)
-    print(clean_text)
-    print(f" length:{len(clean_text)}")
     for symbol in clean_text:
-        symbol_id = symbol_to_id[symbol]
+        symbol_id = _symbol_to_id[symbol]
         sequence += [symbol_id]
-    sequence = sequence + [_symbol_to_id[_pad]]
-    print(f" length:{len(sequence)}")
+    
+    print(clean_text)
+    seq_to_text = sequence_to_text(sequence)
+    print(seq_to_text)
     return sequence
 
 
-def cleaned_text_to_sequence(cleaned_text, symbols):
-    '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+def cleaned_text_to_sequence(cleaned_text):
+    """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
     Args:
       text: string to convert to a sequence
     Returns:
       List of integers corresponding to the symbols in the text
-    '''
-    symbol_to_id = {s: i for i, s in enumerate(symbols)}
-    sequence = [symbol_to_id[symbol] for symbol in cleaned_text if symbol in symbol_to_id.keys()]
-    # sequence = sequence + [_symbol_to_id[_pad]]
+    """
+    # print(cleaned_text)
+    sequence = [_symbol_to_id[symbol] for symbol in cleaned_text]
     return sequence
 
 
 def sequence_to_text(sequence):
-  '''Converts a sequence of IDs back to a string'''
-  result = ''
-  for symbol_id in sequence:
-    s = _id_to_symbol[symbol_id]
-    result += s
-  return result
+    """Converts a sequence of IDs back to a string"""
+    result = ""
+    for symbol_id in sequence:
+        s = _id_to_symbol[symbol_id]
+        result += s
+    return result
 
 
 def _clean_text(text, cleaner_names):
-  for name in cleaner_names:
-    cleaner = getattr(cleaners, name)
-    if not cleaner:
-      raise Exception('Unknown cleaner: %s' % name)
-    text = cleaner(text)
-  return text
+    for name in cleaner_names:
+        cleaner = getattr(cleaners, name)
+        if not cleaner:
+            raise Exception("Unknown cleaner: %s" % name)
+        text = cleaner(text)
+    return text
--- text/cleaners.py
+++ text/cleaners.py
@@ -1,66 +1,90 @@
 import re
+import os
 from unidecode import unidecode
 from .numbers import normalize_numbers
-from num2words import num2words
+from dp.phonemizer import Phonemizer
+phonemizer = Phonemizer.from_checkpoint('en_us_cmudict_ipa_forward.pt')
+
 
 
 # Regular expression matching whitespace:
-_whitespace_re = re.compile(r'\s+')
+_whitespace_re = re.compile(r"\s+")
 
 # List of (regular expression, replacement) pairs for abbreviations:
-_abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-    ('mrs', 'misess'),
-    ('mr', 'mister'),
-    ('dr', 'doctor'),
-    ('st', 'saint'),
-    ('co', 'company'),
-    ('jr', 'junior'),
-    ('maj', 'major'),
-    ('gen', 'general'),
-    ('drs', 'doctors'),
-    ('rev', 'reverend'),
-    ('lt', 'lieutenant'),
-    ('hon', 'honorable'),
-    ('sgt', 'sergeant'),
-    ('capt', 'captain'),
-    ('esq', 'esquire'),
-    ('ltd', 'limited'),
-    ('col', 'colonel'),
-    ('ft', 'fort'),
-]]
+_abbreviations = [
+    (re.compile("\\b%s\\." % x[0], re.IGNORECASE), x[1])
+    for x in [
+        ("mrs", "misess"),
+        ("mr", "mister"),
+        ("dr", "doctor"),
+        ("st", "saint"),
+        ("co", "company"),
+        ("jr", "junior"),
+        ("maj", "major"),
+        ("gen", "general"),
+        ("drs", "doctors"),
+        ("rev", "reverend"),
+        ("lt", "lieutenant"),
+        ("hon", "honorable"),
+        ("sgt", "sergeant"),
+        ("capt", "captain"),
+        ("esq", "esquire"),
+        ("ltd", "limited"),
+        ("col", "colonel"),
+        ("ft", "fort"),
+        ('inc', 'incorporated'),
+    ]
+]
 
-def convert_to_ascii(text):
-    return unidecode(text)
-
-def lowercase(text):
-    return text.lower()
-
-def expand_numbers(text):
-    return normalize_numbers(text)
-
-def collapse_whitespace(text):
-    return re.sub(_whitespace_re, ' ', text)
 
 def expand_abbreviations(text):
     for regex, replacement in _abbreviations:
         text = re.sub(regex, replacement, text)
     return text
+
+
+def expand_numbers(text):
+    return normalize_numbers(text)
+
+
+def lowercase(text):
+    return text.lower()
+
+
+def collapse_whitespace(text):
+    return re.sub(_whitespace_re, " ", text)
+
+
+def convert_to_ascii(text):
+    return unidecode(text)
+
+def do_ipa(text):
+    text = text.split()
+    new_text = []
+    for t in text:
+        if t.startswith('*'):
+            t = t.replace("*", "")
+            new_text.append(t)
+        else:    
+            t = phoneme_text(t)
+            new_text.append(t)
     
-def ordinal_dates(text):
-    texts = re.findall("(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(\.)?(\w*)?(\.)?(\s*\d{0,2}\s*),(\s*\d{4})", text)
-    for result in texts:
-        rs = result[0] + result[2] + result[4]
-        word_date = num2words(result[4], to='ordinal')
-        rss = result[0] + result[2] + " " + word_date
-        text = text.replace(rs, rss)
-    return text
+    return ' '.join(new_text)
+
+
+def phoneme_text(text, lang='en_us'):
+    text = phonemizer(text, lang)
+    return text.strip()
+
 
 def english_cleaners(text):
     '''Pipeline for English text, including number and abbreviation expansion.'''
+    text = text.strip()
     text = convert_to_ascii(text)
-    text = ordinal_dates(text)
     text = lowercase(text)
     text = expand_numbers(text)
     text = expand_abbreviations(text)
+    text = collapse_whitespace(text)
+    text = phoneme_text(text)
     text = collapse_whitespace(text)
     return text
